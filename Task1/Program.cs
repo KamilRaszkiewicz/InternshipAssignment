1b3dfd7e7539f707827e0c4bea145d7cc6b1f875
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-02-07 14:15:21 +0100

    Done!

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int hours;
+                int minutes;
+                try
+                {
+                    Console.Write("Pass hours: ");
+                    hours = int.Parse(Console.ReadLine());
+                    if (hours < 0 || hours > 23) throw new Exception();
+
+                    Console.Write("Pass minutes: ");
+                    minutes = int.Parse(Console.ReadLine());
+                    if(minutes < 0 || minutes > 59) throw new Exception();
+                } 
+                catch
+                {
+                    Console.WriteLine("Pass proper input!\n");
+
+                    continue;
+                }
+
+                Console.WriteLine($"Angle between hours arrow and minutes arrow: { GetAngle(hours, minutes) } degrees\n");
+            }
+        }
+
+        private static int GetAngle(int hours, int minutes)
+        {
+            var hoursAngle = (hours % 12) * 360 / 12;
+            var minutesAngle = (minutes) * 360 / 60;
+
+            var degrees = hoursAngle - minutesAngle;
+
+            if (degrees < 0) degrees *= -1;
+            if (degrees > 180) degrees = 360 - degrees;
+
+            return degrees;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using Task23;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var branch = new Branch()
+            {
+                Branches = new List<Branch>()
+                {
+                    new Branch()
+                    {
+                        Branches = new List<Branch>()
+                        {
+                            new Branch() {}
+                        }
+                    },
+                    new Branch()
+                    {
+                        Branches = new List<Branch>()
+                        {
+                            new Branch()
+                            {
+                                Branches= new List<Branch>()
+                                {
+                                    new Branch()
+                                }
+                            },
+                            new Branch()
+                            {
+                                Branches = new List<Branch>()
+                                {
+                                    new Branch()
+                                    {
+                                        Branches = new List<Branch>()
+                                        {
+                                            new Branch()
+                                        }
+                                    },
+                                    new Branch()
+                                }
+                            },
+                            new Branch()
+                        }
+                    }
+                }
+            };
+
+            Console.WriteLine($"Depth: {GetDepth(branch)}");
+        }
+
+        private static int GetDepth(Branch branch)
+        {
+            if (branch.Branches == null) return 1;
+
+            var subDepths = new List<int>();
+            
+            foreach(var child in branch.Branches)
+            {
+                subDepths.Add(GetDepth(child));
+            }
+
+            return subDepths.Max() + 1;
+        } 
+    }
+}
